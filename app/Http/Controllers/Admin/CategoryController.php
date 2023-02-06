f0e1ef8c78fc6aab6e344746a33b65d1e7b2fbd7
MicheleBenaco <benaco.michele@yahoo.it>
2023-02-06 16:44:55 +0100

    Category-validation

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+
+
 
 class CategoryController extends Controller
 {
@@ -35,9 +39,18 @@ class CategoryController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreCategoryRequest $request)
     {
-        //
+        $val = $request->validated();
+        $slug = Category::generateSlug($request->name);
+        $val['slug'] = $slug;
+
+       Category::create($val);
+    //    if($request->has('categories')){
+    //         $newCategory->categories()->attach($request->categories);
+    //    };
+
+        return redirect()->back()->with('message', "Category $slug added successfully");
     }
 
     /**
@@ -69,9 +82,14 @@ class CategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UpdateCategoryRequest $request, Category $category)
     {
-        //
+        $val_data = $request->validated();
+        $slug = Category::generateSlug($request->name);
+        $val_data['slug'] = $slug;
+        $type->update($val_data);
+
+        return redirect()->back()->with('message', "Category $slug updated successfully");
     }
 
     /**
--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,17 +1,40 @@
 @extends('layouts.admin')
 @section('content')
+
+@if ($errors->any())
+<div class="alert alert-danger mb-3 mt-3">
+    @foreach ($errors->all() as $error)
+            <div>{{ $error }}</div>
+    @endforeach
+</div> 
+@endif
+
 <h1>index category</h1>
+
+<form action="{{route('admin.categories.store')}}" method="post" class="d-flex align-items-center">
+    @csrf
+    <div class="input-group mb-3">
+        <input type="text" name="name" class="form-control" placeholder="Add a category name here " aria-label="Recipient's username" aria-describedby="button-addon2">
+        <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Add</button>
+    </div>
+</form>
+
+@if(session()->has('message'))
+<div class="alert alert-success mb-3 mt-3">
+    {{ session()->get('message') }}
+</div>
+@endif
+
     
     {{-- <p>{{$category->name}}</p> --}}
    
-    <table class="table table-dark">
+    <table class="table table-dark table-striped">
       
         <thead>
           <tr>
             <th scope="col">#</th>
             <th scope="col">Name</th>
-            <th scope="col"></th>
-            <th scope="col"></th>
+            <th scope="col">Plate</th>
           </tr>
         </thead>
         <tbody>
@@ -19,8 +42,7 @@
           <tr>
             <th scope="row">{{$category->id}}</th>
             <td>{{$category->name}}</td>
-            <td></td>
-            <td></td>
+            <td>{{$category->plates && count($category->plates) > 0 ? count($category->plates) : 0}}</td>
           </tr>
           
         </tbody>
