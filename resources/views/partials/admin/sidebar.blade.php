e09e1e0d885ba795a4c603f8b711e53d90b10fd4
colopisalvatore <salvatorecolopi1989@gmail.com>
2023-02-06 19:55:58 +0100

    add logo and change color style

--- resources/views/partials/admin/sidebar.blade.php
+++ resources/views/partials/admin/sidebar.blade.php
@@ -1,26 +1,26 @@
-<nav id="sidebarMenu" class="bg-dark navbar-dark">
-    <h2 class="p-2">Deliveboo</h2>
+<nav id="sidebarMenu" class="bg-light navbar-light ">
+    <img src="{{ asset('img/logo-b-01.png') }}" alt="">
     <ul class="nav flex-column">
-        <li class="nav-item"> <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.dashboard' ? 'bg-secondary' : '' }}" href="{{route('admin.dashboard')}}">
+        <li class="nav-item"> <a class="nav-link text-black {{ Route::currentRouteName() == 'admin.dashboard' ? 'bg-secondary' : '' }}" href="{{route('admin.dashboard')}}">
             <i class="fa-solid fa-house-user fa-lg fa-fw"></i> Dashboard
         </a></li>
         <li class="nav-item">
-            <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.restaurants.index' ? 'bg-secondary' : '' }}" href="{{route('admin.restaurants.index')}}">
+            <a class="nav-link text-black {{ Route::currentRouteName() == 'admin.restaurants.index' ? 'bg-secondary' : '' }}" href="{{route('admin.restaurants.index')}}">
                 <i class="fa-solid fa-utensils fa-lg fa-fw"></i> Restaurant
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.plates.index' ? 'bg-secondary' : '' }}" href="{{route('admin.plates.index')}}">
+            <a class="nav-link text-black {{ Route::currentRouteName() == 'admin.plates.index' ? 'bg-secondary' : '' }}" href="{{route('admin.plates.index')}}">
                 <i class="fa-solid fa-plate-wheat fa-lg fa-fw"></i> Plates
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.categories.index' ? 'bg-secondary' : '' }}" href="{{route('admin.categories.index')}}">
+            <a class="nav-link text-black {{ Route::currentRouteName() == 'admin.categories.index' ? 'bg-secondary' : '' }}" href="{{route('admin.categories.index')}}">
                 <i class="fa-solid fa-bookmark fa-lg fa-fw"></i> Categories
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.types.index' ? 'bg-secondary' : '' }}" href="{{route('admin.types.index')}}">
+            <a class="nav-link text-black {{ Route::currentRouteName() == 'admin.types.index' ? 'bg-secondary' : '' }}" href="{{route('admin.types.index')}}">
                 <i class="fa-solid fa-tag fa-lg fa-fw"></i> Types
             </a>
         </li>
