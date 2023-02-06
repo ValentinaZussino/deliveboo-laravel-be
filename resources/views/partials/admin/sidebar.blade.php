d53104488da9036afd72572ebaad8d43e46d536c
colopisalvatore <salvatorecolopi1989@gmail.com>
2023-02-06 19:33:17 +0100

    cambiate le icone alla sidebar

--- resources/views/partials/admin/sidebar.blade.php
+++ resources/views/partials/admin/sidebar.blade.php
@@ -2,16 +2,16 @@
     <h2 class="p-2">Deliveboo</h2>
     <ul class="nav flex-column">
         <li class="nav-item"> <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.dashboard' ? 'bg-secondary' : '' }}" href="{{route('admin.dashboard')}}">
-            <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Dashboard
+            <i class="fa-solid fa-house-user fa-lg fa-fw"></i> Dashboard
         </a></li>
         <li class="nav-item">
             <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.restaurants.index' ? 'bg-secondary' : '' }}" href="{{route('admin.restaurants.index')}}">
-                <i class="fa-solid fa-newspaper fa-lg fa-fw"></i> Restaurant
+                <i class="fa-solid fa-utensils fa-lg fa-fw"></i> Restaurant
             </a>
         </li>
         <li class="nav-item">
             <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.plates.index' ? 'bg-secondary' : '' }}" href="{{route('admin.plates.index')}}">
-                <i class="fa-solid fa-folder-open fa-lg fa-fw"></i> Plates
+                <i class="fa-solid fa-plate-wheat fa-lg fa-fw"></i> Plates
             </a>
         </li>
         <li class="nav-item">
@@ -21,7 +21,7 @@
         </li>
         <li class="nav-item">
             <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.types.index' ? 'bg-secondary' : '' }}" href="{{route('admin.types.index')}}">
-                <i class="fa-solid fa-bookmark fa-lg fa-fw"></i> Types
+                <i class="fa-solid fa-tag fa-lg fa-fw"></i> Types
             </a>
         </li>
         {{-- <li class="nav-item">
