73a9bc0aa7d1c5fa807c3b524acee416b7d17ce2
colopisalvatore <salvatorecolopi1989@gmail.com>
2023-02-06 12:26:34 +0100

    reworkata la dashboard.blade.php ed aggiunta la sidebar laterale

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,27 +1,32 @@
 @extends('layouts.app')
 
-@section('content')
-@include('partials.admin.sidebar')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<div id="admin">
+    @include('partials.admin.sidebar')
+    <div id="wrapper-admin">
+        @section('content')
 
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
+        <div class="container mt-5">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">{{ __('Dashboard') }}</div>
+        
+                        <div class="card-body">
+                            @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                            @endif
+        
+                            {{ __('You are logged in!') }}
+                        </div>
                     </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
                 </div>
             </div>
+            <div class="mt-5 text-center">
+                <h1>{{$restaurant->name}}</h1>
+            </div>
         </div>
-    </div>
-    <div class="mt-5 text-center">
-        <h1>{{$restaurant->name}}</h1>
     </div>
 </div>
 @endsection
