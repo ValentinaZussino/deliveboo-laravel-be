8740c04e9cea638937c62648e9f9d5e17a9ca40a
MicheleBenaco <benaco.michele@yahoo.it>
2023-02-06 12:34:10 +0100

    tabella-category

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,7 +1,30 @@
 @extends('layouts.admin')
 @section('content')
 <h1>index category</h1>
-    @foreach ($categories as $category)
-    <p>{{$category->name}}</p>
-    @endforeach
+    
+    {{-- <p>{{$category->name}}</p> --}}
+   
+    <table class="table table-dark">
+      
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col"></th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($categories as $category)
+          <tr>
+            <th scope="row">{{$category->id}}</th>
+            <td>{{$category->name}}</td>
+            <td></td>
+            <td></td>
+          </tr>
+          
+        </tbody>
+        @endforeach
+      </table>
+   
 @endsection
