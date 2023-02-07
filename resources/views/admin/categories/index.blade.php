2869e3bffc88d35742513155dfff1159d6346517
LuisGonzalez27 <luis27coding@gmail.com>
2023-02-07 16:57:37 +0100

    Sistemazione categories e types

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -7,65 +7,24 @@
         <div class="row justify-content-between align-items-center gy-2">
             {{-- TITOLO --}}
             <div class="col-sm-4 col-md-6 col-lg-8">
-                <h5 class="px-2 gap-1 mb-0">Add New Category</h5>
-            </div>
-
-            {{-- CAMPO DI INPUT PER CREARE UNA NUOVA RIGA --}}
-            <div class="col-sm-8 col-md-6 col-lg-4 px-4">
-                <form action="{{route('admin.categories.store')}}" method="post" class="d-flex align-items-center">
-                    @csrf
-                    <div class="input-group mb-3">
-                        <input type="text" name="name" class="form-control" placeholder="Add a type name here " aria-label="Recipient's username" aria-describedby="button-addon2">
-                        <button class="btn btn-outline-success" type="submit" id="button-addon2">Add</button>
-                    </div>
-                </form>
+                <h3 class="px-2 gap-1 mb-0 ms-5">Categories</h5>
             </div>
         </div>
     </div>
 
-            {{-- ALERT --}}
-            @if(session()->has('message'))
-            <div class="alert alert-success mb-3 mt-3">
-                {{ session()->get('message') }}
-            </div>
-            @endif
-            @if ($errors->any())
-                <div class="alert alert-danger mb-3 mt-3">
-                    @foreach ($errors->all() as $error)
-                            <div>{{ $error }}</div>
-                    @endforeach   
-                </div> 
-            @endif
+    {{-- LISTA --}}
+    <div class="container-fluid pt-2">
+        <div class="d-flex flex-wrap">
+            @foreach($categories as $category)
 
-            {{-- TABELLA --}}
-        <div class="py-4">
-            <div class="table-responsive datatable-custom">
-                <table class="table table-hover table-borderless table-thead-bordered table-nowrap table-align-middle card-table">
-                    <thead class="thead-light">
-                        <tr class="table-active">
-                            <th scope="col">#</th>
-                            <th scope="col">Name</th>
-                            {{-- <th scope="col">Plates</th> --}}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($categories as $category)
-                                <tr>
-                                    <th>
-                                        {{$category->id}}
-                                    </th>
-                                    <td>
-                                        {{$category->name}}
-                                    </td>
-                                    {{-- <td>
-                                        {{$category->plates && count($category->plates) > 0 ? count($category->plates) : 0}}
-                                    </td> --}}
-                                </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+            <div class="column-2">
+                <div class="card-2 text-capitalize">{{$category->name}}</div>
             </div>
-        </div>   
+
+            @endforeach
+        </div>
+    </div>
+
   <div class="px-4">
     {{ $categories->links('vendor.pagination.bootstrap-5') }}
   </div>
--- resources/views/admin/types/index.blade.php
+++ resources/views/admin/types/index.blade.php
@@ -8,66 +8,26 @@
                     <div class="row justify-content-between align-items-center gy-2">
                         {{-- TITOLO --}}
                         <div class="col-sm-4 col-md-6 col-lg-8">
-                            <h5 class="px-2 gap-1 mb-0">Add New Types</h5>
-                        </div>
-
-                        {{-- CAMPO DI INPUT PER CREARE UNA NUOVA RIGA --}}
-                        <div class="col-sm-8 col-md-6 col-lg-4 px-4">
-                            <form action="{{route('admin.types.store')}}" method="post" class="d-flex align-items-center">
-                                @csrf
-                                <div class="input-group mb-3">
-                                    <input type="text" name="name" class="form-control" placeholder="Add a type name here " aria-label="Recipient's username" aria-describedby="button-addon2">
-                                    <button class="btn btn-outline-success" type="submit" id="button-addon2">Add</button>
-                                </div>
-                            </form>
-                        </div>
+                            <h3 class="px-2 gap-1 mb-0 ms-5">Types</h5>
                     </div>
                 </div>
+            </div>
 
-                        {{-- ALERT --}}
-                        @if(session()->has('message'))
-                        <div class="alert alert-success mb-3 mt-3">
-                            {{ session()->get('message') }}
-                        </div>
-                        @endif
-                        @if ($errors->any())
-                            <div class="alert alert-danger mb-3 mt-3">
-                                @foreach ($errors->all() as $error)
-                                        <div>{{ $error }}</div>
-                                @endforeach   
-                            </div> 
-                        @endif
+            {{-- LISTA --}}
+            <div class="container-fluid pt-2">
+                <div class="d-flex flex-wrap">
+                    @foreach($types as $type)
+        
+                    <div class="column-2">
+                        <div class="card-2 text-capitalize">{{ $type->name }}</div>
+                    </div>
+        
+                    @endforeach
+                </div>
+            </div>
 
-                        {{-- TABELLA --}}
-                    <div class="py-4">
-                        <div class="table-responsive datatable-custom">
-                            <table class="table table-hover table-borderless table-thead-bordered table-nowrap table-align-middle card-table">
-                                <thead class="thead-light">
-                                    <tr class="table-active">
-                                        <th scope="col">#</th>
-                                        <th scope="col">Name</th>
-                                        {{-- <th scope="col">Restaurants</th> --}}
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach($types as $type)
-                                            <tr>
-                                                <th>
-                                                    {{$type->id}}
-                                                </th>
-                                                <td>
-                                                    {{$type->name}}
-                                                </td>
-                                                {{-- <td>
-                                                    {{$type->restaurants && count($type->restaurants) > 0 ? count($type->restaurants) : 0}}
-                                                </td> --}}
-                                            </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    <div class="px-4">
-                        {{ $types->links('vendor.pagination.bootstrap-5') }}
-                    </div>
+            <div class="px-4">
+                {{ $types->links('vendor.pagination.bootstrap-5') }}
+            </div>
+        
 @endsection
