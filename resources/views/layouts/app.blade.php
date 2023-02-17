bd4e7d519d065da57d03967cfc846467baa3b746
MicheleBenaco <benaco.michele@yahoo.it>
2023-02-17 09:46:36 +0100

    fixheader

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,12 +35,14 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
                         <li class="nav-item">
                             <a class="nav-link" href="{{url('/') }}">{{ __('Home') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link"  href="http://localhost:5174/">Vai al sito  </a>
                         </li>
                     </ul>
 
--- resources/views/partials/admin/header.blade.php
+++ resources/views/partials/admin/header.blade.php
@@ -22,6 +22,7 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{url('/') }}">{{ __('Home') }}</a>
                 </li>
+
             </ul>
 
             <!-- Right Side Of Navbar -->
